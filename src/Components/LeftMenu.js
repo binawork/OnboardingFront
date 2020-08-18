8eb4b34dd0fa806574ec4f38af6d3e5a7859c29d
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-08-18 17:31:42 +0200

    Add body to Left menu according to bootstrap templates

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -3,9 +3,18 @@ import React from "react";
 
 function LeftMenu() {
     return(
-        <div>
+        <aside className="app-aside app-aside-expand-md app-aside-light">
+            <div className="aside-content">
+                <header className="aside-header d-block d-md-none">
 
-        </div>
+                </header>
+                <div className="aside-menu overflow-hidden">
+                    <nav id="stacked-menu" className="stacked-menu">
+
+                    </nav>
+                </div>
+            </div>
+        </aside>
     )
 }
 export default LeftMenu;
