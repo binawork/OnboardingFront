e357b51999ac1e6b8b5cc4db4f27aba256e61d29
Janek Pachnk <januzpachnik@gmail.com>
2020-08-18 11:25:04 +0200

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer>
+            yayaya
+        </footer>
+    )
+}
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return(
+        <ul>
+            <li>Jeden</li>
+            <li>Dwa</li>
+            <li>Trzy</li>
+            <li>Cztery</li>
+        </ul>
+    )
+}
+export default Header;
