c529140cbfc8490b166f60cc36d0c008a4125ea2
Janek Pachnk <januzpachnik@gmail.com>
2020-09-08 15:13:27 +0200

    added adresses for basic routing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 //import UserListRow from "../Components/UsersList/UserListRow";
 import DashboardPage from "./Components/DashboardPage";
 import UserListPage from "./Components/UsersList/UserListPage";
-import FormsEditPage from "./Components/FormsEditPage";
+import FormsEditPage from "./Components/FormsEdit/FormsEditPage";
 import FormTablePage from "./Components/FormTable/FormTablePage";
 import AddUserTablePage from "./Components/AddUserTable/AddUserTablePage";
 import UserManagerProfilePage from "./Components/UserManagerProfilePage";
@@ -19,12 +19,19 @@ import EmployePageFillCopyPage from "./Components/EmployePageFillCopy/EmployePag
 import Navbar from "./Components/Navbar";
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Route path="/profile/manager" component={ UserManagerProfilePage } />
-        <Route path="/profile/employee" component={ EmployeProfilePage } />
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Route path="/profile/manager" component={ UserManagerProfilePage } />
+            <Route path="/profile/employee" component={ EmployeProfilePage } />
+            <Route path="/add_user" component={ EmployePageFillCopyPage } />
+            <Route path="/employe_page_fill" component={ EmployeProfilePage } />
+            <Route path="/form_edit" component={ FormsEditPage } />
+            <Route path="/form_table" component={ FormTablePage } />
+            <Route path="/user_list" component={ UserListPage } />
+            <Route path="/" component={ DashboardPage } />
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
