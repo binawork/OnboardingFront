5dda12dcf25175918e4ea57fb8b762eb2147886a
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-09-14 11:46:31 +0200

    Temporary add components list

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -22,10 +22,10 @@ function LeftMenu() {
                             <Link to='/packages' className="menu-link"><span className="menu-icon far fa-file"></span> <span className="menu-text">Wdrożenia</span></Link>
                             <ul className="menu">
                                 <li className="menu-item">
-                                    <a href="#" className="menu-link">Lista formularzy wdrożeń</a>
+                                    <Link to="/package_page" className="menu-link">Lista formularzy</Link>
                                 </li>
                                 <li className="menu-item">
-                                    <a href="#" className="menu-link">- dodaj pracownika</a>
+                                    <Link to="/packages" className="menu-link">- wyślij pracownikowi</Link>
                                 </li>
                             </ul>
                         </li>
@@ -38,6 +38,46 @@ function LeftMenu() {
                         </li>
                       </ul>
 
+                      <ul className="menu">
+                        <li className="menu-header">
+                            <span className="menu-icon fas fa-home"></span> <span className="menu-text">Components</span>
+                        </li>
+
+                        <li className="menu-item">
+                            <Link to='/add_user' className="menu-link"><span className="menu-icon fas fa-file"></span> <span className="menu-text">Add new user</span></Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to='/user_list' className="menu-link"><span className="menu-icon fas fa-file"></span> <span className="menu-text">Lista pracowników</span></Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/profile/manager" className="menu-link"> UserManagerProfile</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/profile/employee" className="menu-link"> EmployeProfile</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/add_user" className="menu-link"> UserManagerProfile</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/form_edit" className="menu-link"> FormsEdit</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/package_page" className="menu-link"> FormTable</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/user_list" className="menu-link"> UserList</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/packages" className="menu-link"> AddUserTable </Link>
+                        </li>
+
+                        <li className="menu-item">
+                            <Link to="/employe_forms_list" className="menu-link"> EmployePageFillCopy</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/employe_page_fill" className="menu-link"> FormsEmployee</Link>
+                        </li>
+                      </ul>
                     </nav>
                 </div>
                 <ModeButton />
