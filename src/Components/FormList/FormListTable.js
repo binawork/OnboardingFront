2498697a12dc6448347895be632317d691e3a344
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-09-22 10:20:31 +0200

    Add AJAX hooks component folder for connection methods. Use packages hook in packages table

--- src/Components/FormList/FormListTable.js
+++ src/Components/FormList/FormListTable.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React/*, { useState, useEffect }*/ from "react";
 import PackagesAddNew from "./PackagesAddNew";
-import PackagesRow from "./PackagesRow";
-import { formDataList } from "../FormTable/FormTableData";
-import { getPath } from "../utils.js";
-
+//import PackagesRow from "./PackagesRow";
+//import { formDataList } from "../FormTable/FormTableData";
+//import { getPath } from "../utils.js";
+import Packages from "../hooks/Packages";
 
 function FormListTable() {
-	var [rows , setRows] = useState([]),
+	/*var [rows , setRows] = useState([]),
 		[loaded, isLoaded] = useState(false);
 	let form_table= [], url = getPath();
 	let fetchProps = {method:"GET", headers:{"Accept":"application/json", "Content-Type":"application/json", "X-CSRFToken":""}};
 
-	useEffect(() => {
+	/*useEffect(() => {
 		fetch(url + "api/package/", fetchProps).then(res => res.json()).then(
 			(result) => {
 				isLoaded(true);
@@ -29,7 +29,7 @@ function FormListTable() {
         rows.forEach(function (element) {
             form_table.push(<PackagesRow key = { element.id } row={ {name: element['title'], last_edit:element['updated_on']} }/>)
         });
-    }
+    }*/
     return(
         <div className="page-section">
             <div className="card card-fluid">
@@ -54,7 +54,7 @@ function FormListTable() {
                         </tr>
                         </thead>
                         <tbody id="form_table_data_container">
-                            { form_table }
+                            { <Packages /> }
                         </tbody>
                     </table>
                 </div>
